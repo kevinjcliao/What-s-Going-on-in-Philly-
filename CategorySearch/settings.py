91a6ae3dc08304f2b09b65998154b8fdbb4ac3db
Kevin Jiah-Chih Liao <me@liaokev.in>
2015-03-17 17:49:27 -0400

    Category Search now serving 'hello world' in index.

--- CategorySearch/settings.py
+++ CategorySearch/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+PROJECT_PATH = os.path.abspath(os.path.dirname(__name__)) 
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -36,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CategorySearch'
 )
 
 MIDDLEWARE_CLASSES = (
--- CategorySearch/urls.py
+++ CategorySearch/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from CategorySearch import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'CategorySearch.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.index, name = 'index'), 
     url(r'^admin/', include(admin.site.urls)),
 )
