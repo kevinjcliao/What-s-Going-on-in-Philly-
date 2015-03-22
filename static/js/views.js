40b43f4bee45f941ff612040ffad81ef89b039a5
Kevin Jiah-Chih Liao <me@liaokev.in>
2015-03-22 19:22:34 -0400

    Front-end result_event card enhancements.

--- static/js/views.js
+++ static/js/views.js
@@ -30,9 +30,8 @@ $(document).ready( function(){
 	
 				$('#loading').hide('slow'); 
 				console.log('displayEvents: Callback!'); 
-				$('#events').show('slow'); 	
 				$('#follow_up_action').show('slow'); 
-				current_counter += 5; 
+				current_counter += 6; 
 			}); //End displayEvents
 			
 			$(window).scroll(function (){
@@ -40,8 +39,7 @@ $(document).ready( function(){
 					console.log(events); 
 					displayEvents(current_counter, events, function(){
 						console.log("Showing five more events."); 
-						('#events').show('slow'); 
-						current_counter += 5; 
+						current_counter += 6; 
 					}); 
 				}
 			}); 
@@ -56,27 +54,44 @@ $(document).ready( function(){
 }); //End document.ready
 
 function displayEvents(initial_counter, events, callback){
+	console.log('initial_counter: ' + initial_counter); 
+
+	// Collects information from the events list
 	var event_name, event_time, event_location, fragment_to_append; 
+
+	// Selects a rational maximum amount of events to load before more
+	// scrolling is required. 
 	var maximum_display_counter = initial_counter+5; 
+
 	for (i=initial_counter; i<=maximum_display_counter; i++){
+		// Variable creation: 
 		event_name = events[i]["name"]["html"]; 
 		event_location = events[i]["venue"]["address"]["address_1"]; 
+		event_url = events[i]["url"]; 
 		event_time = null; 
+
+
 		if(event_location==null){
 			event_location = "Location unspecified"; 
 		}
 
-		fragment_to_append = "<div id='result_event" + i + "'" + "class=result_event>" + 
-			"<h3>" + event_name + "</h3>" +
+		fragment_to_append = "<div id='result_event" + i + "'" + 
+			"class=result_event style='display: none'>" + 
+			"<div id='event_left'>" +
+			"<h3><a href='" + event_url + "'>" + event_name + "</a></h3>" +
+			"</div>" +
+			"<div id='event_right'>" +
 			// Google Maps API Request
 			"<iframe id='map' width='300' height='300' frameborder='0' style='border:0' "+ 
 			"src='https://www.google.com/maps/embed/v1/place?key=" + MAP_API_KEY +
 			"&q=" + event_location + ", Philadelphia'>" + 
 			"</iframe>" + "<br>" +
 			"<p class='result_location_name'>" + event_location + "</p>"  +
+			"</div>" +
 			"</div>"; 
 		console.log("appending " + fragment_to_append); 
 		$('#events').append(fragment_to_append); 
+		$('#result_event'+i).show('slow'); 
 	}
 
 	// Give Google Maps requests time to load. 	
